80e4662bc52f549910f5df516daa9fe5069a58a2
Khwost <maaferak@gmail.com>
2022-10-12 09:29:40 +0200

    Тестовый круг внутри светофора

--- TrafficLights/TrafficLights/Views/TrafficLightsCanvas.axaml.cs
+++ TrafficLights/TrafficLights/Views/TrafficLightsCanvas.axaml.cs
@@ -1,11 +1,32 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
+using System.Reflection.PortableExecutable;
 
 namespace TrafficLights.Views
 {
     public partial class TrafficLightsCanvas : UserControl
     {
+        /// <summary>
+        /// Цвет корпуса
+        /// </summary>
+        public readonly IBrush CaseColor = Brushes.Black;
+
+        /// <summary>
+        /// Цвет кругов огней
+        /// </summary>
+        public readonly IBrush CircleColor = Brushes.Black;
+
+        /// <summary>
+        /// Ширина линии корпуса
+        /// </summary>
+        private const double CaseLinesWidth = 5; 
+
+        /// <summary>
+        /// Ширина линии кругов
+        /// </summary>
+        public const double CirclesLinesWidth = 2;
+
         public TrafficLightsCanvas()
         {
             InitializeComponent();
@@ -16,14 +37,26 @@ namespace TrafficLights.Views
         /// </summary>
         public override void Render(DrawingContext context)
         {
-            var point1 = new Point(0, 0);
-            var point2 = new Point(100, 100);
+            var width = Width;
+            var height = Height;
 
-            var pen = new Pen(Brushes.Green, 1, lineCap: PenLineCap.Square);
+            // Рисуем корпус светофора
+            var casePen = new Pen(CaseColor, CaseLinesWidth, lineCap: PenLineCap.Square);
+            context.DrawRectangle(casePen, new Rect(0, 0, width, height));
 
-            context.DrawLine(pen, point1, point2);
+            // Рисуем круги
+            var circlePen = new Pen(CircleColor, CirclesLinesWidth, lineCap: PenLineCap.Square);
+            DrawCircle(context, 50, 50, 10, circlePen);
 
             base.Render(context);
+        }
+
+        /// <summary>
+        /// Рисование круга
+        /// </summary>
+        private void DrawCircle(DrawingContext context, double x, double y, double radius, Pen pen)
+        {
+            context.DrawEllipse(Brushes.Transparent, pen, new Point(x, y), radius, radius);
         }
     }
 }
